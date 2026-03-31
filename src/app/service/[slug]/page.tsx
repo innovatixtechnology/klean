e4384e3bb736017bf70cb06f0c9ada220e3a5af9
Rishank Yashu <vinayakaproject23@gmail.com>
2026-04-01 00:01:15 +0530

    fix build

--- src/app/service/[slug]/page.tsx
+++ src/app/service/[slug]/page.tsx
@@ -2,12 +2,26 @@ import { notFound } from "next/navigation";
 import { CATEGORY } from "@/constants";
 import ComingSoon from "@/components/ComingSoon";
 
-export const metadata = {
-  title: 'Service',
-};
 interface IProps {
   params: Promise<{ slug: string }>;
 }
+
+export const generateStaticParams = async () => {
+  return CATEGORY.map((cat) => ({
+    slug: cat.slug,
+  }));
+};
+
+export const generateMetadata = async ({ params }: IProps) => {
+  const slug = (await params).slug
+  const service = CATEGORY.find(s => s.slug === slug);
+  if (!service) return {};
+
+  return {
+    title: `${service.text}`,
+    description: service.text,
+  };
+};
 
 export default async function Service({ params }: IProps) {
   const { slug } = await params;
