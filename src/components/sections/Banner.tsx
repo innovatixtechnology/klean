67157de8449c45b7c1b956a5f94c5d0edfc3119d
AashishKumar-3002 <aashish201810kumar@gmail.com>
2026-04-10 08:10:53 +0530

    images changes

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -4,11 +4,11 @@ export default function Banner() {
   return (
     <section id="banner" className="mt-20 px-4 md:px-10 lg:max-w-[80%] mx-auto">
       <div className="flex flex-col lg:flex-row gap-6 md:gap-4">
-        <div className="relative w-full aspect-video md:aspect-[1280/720]">
-          <Image src="/images/hydra-facial-treatment1.webp" alt="corporate wellness" fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover rounded-3xl" />
+        <div className="relative w-full aspect-video md:aspect-1280/720">
+          <Image src="/images/servicebanner1.png" alt="HydraGlo facial treatments banner" fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover rounded-3xl" />
         </div>
-        <div className="relative w-full aspect-video md:aspect-[1280/720]">
-          <Image src="/images/Laser-Banner.webp" alt="corporate wellness" fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover rounded-3xl" />
+        <div className="relative w-full aspect-video md:aspect-1280/720">
+          <Image src="/images/servicebanner2.png" alt="Laser hair reduction banner" fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover rounded-3xl" />
         </div>
       </div>
     </section>
